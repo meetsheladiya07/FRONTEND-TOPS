a59fc4ff3973579381630643789142fd0dfe9650
meetsheladiya07 <meetsheladiya567@gmail.com>
2026-08-20 19:06:21 +0530

    js

---
+++ Javascript/ex.js
@@ -0,0 +1,15 @@
+// console.log("hello external js")
+// console.log("sub : ",90-56)
+
+
+// Bom : borwser object model 
+// 1) 
+// alert("pls feild data")
+
+// confirm("are your sure?")
+
+
+// prompt("5 + 5 = ?")
+
+
+// print()
